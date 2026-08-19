8d83f67181795fbed895fb8d69397cfa257d3f1d
caofangkuai <caofangkuai2022@gmail.com>
2026-08-20 01:23:32 +0800

    chore: 移除 PendingIntent FLAG_MUTABLE（仅保留 FLAG_UPDATE_CURRENT）

--- app/src/main/java/com/wunelezi/injector/MainActivity.kt
+++ app/src/main/java/com/wunelezi/injector/MainActivity.kt
@@ -723,7 +723,7 @@ class MainActivity : AppCompatActivity() {
                     // 创建 PendingIntent
                     val pendingIntent = android.app.PendingIntent.getActivity(
                         this, 0, intent2,
-                        android.app.PendingIntent.FLAG_UPDATE_CURRENT or android.app.PendingIntent.FLAG_MUTABLE
+                        android.app.PendingIntent.FLAG_UPDATE_CURRENT
                     )
 
                     // intent1: 指向目标包的 AssistActivity
